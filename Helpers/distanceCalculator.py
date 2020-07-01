99a1372a4e11c6d933cc41fc9388df1ad8a0cd88
Isaac Bly <locke19hegemon@gmail.com>
2020-06-30 18:58:26 -0700

    #2 Distance Calculator

--- Helpers/distanceCalculator.py
+++ Helpers/distanceCalculator.py
@@ -1,3 +1,12 @@
 
-def calculate_distance(x1, y1, z1, x2, y2, z2):
-    pass
+def calculate_distance(system1, system2):
+	x1 = system1[0]
+	y1 = system1[1]
+	z1 = system1[2]
+	x2 = system2[0]
+	y2 = system2[1]
+	z2 = system2[2]
+
+	distance = sqrt.math((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+	
+	return distance
