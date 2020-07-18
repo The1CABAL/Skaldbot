a4f50f2c7ca1b86e5e5ced654c019afe805f4b86
Isaac Bly <locke19hegemon@gmail.com>
2020-07-18 08:42:15 -0700

    upodate deployment script

--- 01DeployDBO.sh
+++ 01DeployDBO.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+chmod a+x /home/ib-2020/sqlpackage/sqlpackage
+source /home/ib-2020/.bashrc
 cd /var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/bin/Debug
 sqlpackage /Action:Publish /SourceFile:"SkaldBot.Database.dacpac" /Profile:"/var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/Profiles/SkaldBot.Database.publish.xml"
