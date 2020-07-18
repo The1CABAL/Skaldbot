0e98278485f4e33e0b2595bb6f4cb4fa354acd67
Isaac Bly <locke19hegemon@gmail.com>
2020-07-17 21:29:20 -0700

    Deploy Test

--- 01DeployDBO.sh
+++ 01DeployDBO.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-#sqlpackage /Action:Publish /SourceFile:"/var/lib/go-agent/pipelines/SkaldBot-Test", /Profile:"profilePath"
+sqlpackage /Action:Publish /SourceFile:"/var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/bin/Debug/SkaldBot.Database.dacpac", /Profile:"/var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/Profiles/SkaldBot.Database.publish"
