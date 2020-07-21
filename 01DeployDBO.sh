581b88c09fc09d170e9f7aa355769e8f0c1f108c
Isaac Bly <locke19hegemon@gmail.com>
2020-07-21 07:46:00 -0700

    Updated Deploy Bash script

--- 01DeployDBO.sh
+++ 01DeployDBO.sh
@@ -2,5 +2,5 @@
 #echo "export PATH=\"\$PATH:$HOME/sqlpackage\"" >> /home/ib-2020/.bashrc
 #sudo chmod a+x /home/ib-2020/sqlpackage/sqlpackage
 #sudo source /home/ib-2020/.bashrc
-#cd /var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/bin/Debug
+cd /var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/bin/Debug
 sqlpackage /Action:Publish /SourceFile:"SkaldBot.Database.dacpac" /Profile:"/var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/Profiles/SkaldBot.Database.publish.xml"
