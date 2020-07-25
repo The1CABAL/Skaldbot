d5b64c7b43a37bca3407193bdc9d6142a7a8bdde
Isaac Bly <locke19hegemon@gmail.com>
2020-07-24 22:18:21 -0700

    changes to database deployment

---
+++ 01DeployDBO.bat
@@ -0,0 +1,1 @@
+\sqlpackage\sqlpackage.exe /Action:Publish /tsn:"ib-2020.ib.local" /tu:"sa" /tp:"pwd4dsn_ib2020" /tdn:"SkaldBot.Database" /SourceFile:"C:\Program Files (x86)\Go Agent\pipelines\build\SkaldBot.Database\bin\Debug\SkaldBot.Database.dacpac"
--- 01DeployDBO.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#echo "export PATH=\"\$PATH:$HOME/sqlpackage\"" >> /home/ib-2020/.bashrc
-#sudo chmod a+x /home/ib-2020/sqlpackage/sqlpackage
-#sudo source /home/ib-2020/.bashrc
-cd /var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/bin/Debug
-sqlpackage /Action:Publish /SourceFile:"SkaldBot.Database.dacpac" /Profile:"/var/lib/go-agent/pipelines/SkaldBot-DBOTest/SkaldBot.Database/Profiles/SkaldBot.Database.publish.xml"
