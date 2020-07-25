10cb83fc2d2ff4cb4b6f7754ffc04cd31851d47b
Isaac Bly <locke19hegemon@gmail.com>
2020-07-25 08:29:36 -0700

    Update Deployment script

--- 01DeployDBO.bat
+++ 01DeployDBO.bat
@@ -1,1 +1,1 @@
-..\..\sqlpackage\sqlpackage.exe /Action:Publish /tsn:"ib-2020.ib.local" /tu:"sa" /tp:"pwd4dsn_ib2020" /tdn:"SkaldBot.Database" /SourceFile:"\SkaldBot.Database\bin\Debug\SkaldBot.Database.dacpac"
+..\..\sqlpackage\sqlpackage.exe /Action:Publish /tsn:"ib-2020.ib.local" /tu:"sa" /tp:"pwd4dsn_ib2020" /tdn:"SkaldBot.Database" /SourceFile:"C:\Program Files (x86)\Go Agent\pipelines\SkaldBot.Database\bin\Debug\SkaldBot.Database.dacpac"
