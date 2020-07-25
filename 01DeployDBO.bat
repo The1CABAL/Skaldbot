24b0da7e192d31f3b9da11942468a9893fc92ea1
Isaac Bly <locke19hegemon@gmail.com>
2020-07-25 08:38:29 -0700

    Test cwdir in Go

--- 01DeployDBO.bat
+++ 01DeployDBO.bat
@@ -1,1 +1,2 @@
-..\..\sqlpackage\sqlpackage.exe /Action:Publish /tsn:"ib-2020.ib.local" /tu:"sa" /tp:"pwd4dsn_ib2020" /tdn:"SkaldBot.Database" /SourceFile:"C:\Program Files (x86)\Go Agent\pipelines\SkaldBot.Database\bin\Debug\SkaldBot.Database.dacpac"
+dir
+rem ..\..\sqlpackage\sqlpackage.exe /Action:Publish /tsn:"ib-2020.ib.local" /tu:"sa" /tp:"pwd4dsn_ib2020" /tdn:"SkaldBot.Database" /SourceFile:"C:\Program Files (x86)\Go Agent\pipelines\build\SkaldBot.Database\bin\Debug\SkaldBot.Database.dacpac"
