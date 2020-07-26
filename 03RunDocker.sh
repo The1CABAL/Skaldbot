7a62d0b520d56e31a819e5525b1aba9666168544
Isaac Bly <locke19hegemon@gmail.com>
2020-07-25 18:11:47 -0700

    Updated scripts for test deployment

--- 03RunDocker.sh
+++ 03RunDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot && \ 
+sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot-test && \ 
 sleep 5 && \
-sudo docker inspect -f {{.State.Running}} skaldbot && \
+sudo docker inspect -f {{.State.Running}} skaldbot-test && \
 echo "Container is running."
