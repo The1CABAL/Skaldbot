ebb6df77b862c0cf79a0db497e2ca3cb4688c4c1
Isaac Bly <locke19hegemon@gmail.com>
2020-07-26 12:12:55 -0700

    Corrected deployment syntax errors

--- 03RunDocker.sh
+++ 03RunDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot && \ 
 sleep 5 && \
-state = sudo docker inspect -f {{.State.Running}} skaldbot && \
+state = sudo docker inspect -f {{.State.Running}} skaldbot
 if [$state == False] then
 	exit 1
 fi
