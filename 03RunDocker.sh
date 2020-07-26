518d97c18985518d47c824bc9403d18ce3c64b27
Isaac Bly <locke19hegemon@gmail.com>
2020-07-26 12:15:11 -0700

    Corrected bash syntax

--- 03RunDocker.sh
+++ 03RunDocker.sh
@@ -2,6 +2,7 @@
 sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot && \ 
 sleep 5 && \
 state = sudo docker inspect -f {{.State.Running}} skaldbot
-if [$state == False] then
+if [$state == False] 
+then
 	exit 1
 fi
