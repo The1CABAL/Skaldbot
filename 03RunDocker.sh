723475d863575c524c5a2aae58dfedbc33eefa0f
Isaac Bly <locke19hegemon@gmail.com>
2020-07-26 12:44:44 -0700

    Included new post deployment test

--- 03RunDocker.sh
+++ 03RunDocker.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot && \ 
 sleep 5 && \
-state = sudo docker inspect -f {{.State.Running}} skaldbot
-if [$state == False] 
-then
-	exit 1
-fi
+docker exec -t skaldbot echo "Container Is Running!"
--- 04InstallFFmpeg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo docker exec -t skaldbot apt-get update && apt-get install -y ffmpeg
