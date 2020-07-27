08225544b8a67b93914c7ad0fac66fb2f8816cf4
Isaac Bly <locke19hegemon@gmail.com>
2020-07-27 09:11:16 -0700

    added sudo

--- 03RunDocker.sh
+++ 03RunDocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot && docker exec -t skaldbot echo "Container Is Running"
+sudo docker run -it -p 5478:5478 --name skaldbot -d skaldbot && sudo docker exec -t skaldbot echo "Container Is Running"
