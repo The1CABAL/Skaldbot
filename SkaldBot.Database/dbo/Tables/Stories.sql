61959f6adb19b3e892ab311b896c387b5b529417
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-07-28 17:11:09 -0700

    #12 - CodeServers table created and stories / wisdoms table updated to accept the server id.

--- SkaldBot.Database/dbo/Tables/Stories.sql
+++ SkaldBot.Database/dbo/Tables/Stories.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE [dbo].[Stories] (
     [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [ServerId]        INT            NOT NULL,
     [Title]           NVARCHAR (255) NOT NULL,
     [Story]           NVARCHAR (MAX) NOT NULL,
     [WasSubmitted]    BIT            CONSTRAINT [DF_Stories_WasSubmitted] DEFAULT ((0)) NOT NULL,
     [SubmittedItemId] INT            NULL,
     [IsActive]        BIT            CONSTRAINT [DF_Stories_IsActive] DEFAULT ((1)) NOT NULL,
-    [UpdateDate]      DATETIME       NULL,
+    [UpdateDate]      DATETIME       CONSTRAINT [DF_Stories_UpdateDate] DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_Stories] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Stories_SubmittedItems] FOREIGN KEY ([SubmittedItemId]) REFERENCES [dbo].[SubmittedItems] ([Id])
+    CONSTRAINT [FK_Stories_SubmittedItems] FOREIGN KEY ([ServerId]) REFERENCES [dbo].[CodeServers] ([Id])
 );
+
+
 
 
 
--- SkaldBot.Database/dbo/Tables/Wisdoms.sql
+++ SkaldBot.Database/dbo/Tables/Wisdoms.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[Wisdoms] (
     [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [ServerId]        INT            NOT NULL,
     [Title]           NVARCHAR (255) NOT NULL,
     [Wisdom]          NVARCHAR (MAX) NOT NULL,
     [WasSubmitted]    BIT            CONSTRAINT [DF_Wisdoms_WasSubmitted] DEFAULT ((0)) NOT NULL,
     [SubmittedItemId] INT            NULL,
     [IsActive]        BIT            CONSTRAINT [DF_Wisdoms_IsActive] DEFAULT ((1)) NOT NULL,
-    [UpdateDate]      DATETIME       NULL,
+    [UpdateDate]      DATETIME       CONSTRAINT [DF_Wisdoms_UpdateDate] DEFAULT (getdate()) NULL,
     CONSTRAINT [PK_Wisdoms] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Wisdoms_SubmittedItems] FOREIGN KEY ([SubmittedItemId]) REFERENCES [dbo].[SubmittedItems] ([Id])
+    CONSTRAINT [FK_Wisdoms_SubmittedItems] FOREIGN KEY ([ServerId]) REFERENCES [dbo].[CodeServers] ([Id])
 );
+
+
 
