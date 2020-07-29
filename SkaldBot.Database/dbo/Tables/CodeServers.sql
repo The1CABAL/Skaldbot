7ec2888b295ca56401fb3a817324f57f6f7e3975
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-07-28 17:30:36 -0700

    #46 - Accounts table and added references to tables that would require it. Seed created / updated for admin account.

--- SkaldBot.Database/dbo/Tables/CodeServers.sql
+++ SkaldBot.Database/dbo/Tables/CodeServers.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[CodeServers] (
     [Id]         INT            IDENTITY (1, 1) NOT NULL,
     [ServerId]   NVARCHAR (255) NOT NULL,
-    [ClientId]   BIGINT         NULL,
+    [AccountId]  BIGINT         NULL,
     [UpdateDate] DATETIME       CONSTRAINT [DF_CodeServers_UpdateDate] DEFAULT (getdate()) NULL,
-    CONSTRAINT [PK_CodeServers] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_CodeServers] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_CodeServers_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts] ([AccountId])
 );
+
+
 
--- SkaldBot.Database/dbo/Tables/Users.sql
+++ SkaldBot.Database/dbo/Tables/Users.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Users] (
     [Id]           UNIQUEIDENTIFIER CONSTRAINT [DF_Table_1_id] DEFAULT (newid()) NOT NULL,
+    [AccountId]    BIGINT           NULL,
     [Username]     NVARCHAR (255)   NOT NULL,
     [FirstName]    NVARCHAR (255)   NOT NULL,
     [LastName]     NVARCHAR (255)   NOT NULL,
@@ -7,8 +8,11 @@
     [IsLocked]     BIT              CONSTRAINT [DF_Users_IsLocked] DEFAULT ((0)) NOT NULL,
     [IsActive]     BIT              CONSTRAINT [DF_Users_IsActive] DEFAULT ((1)) NOT NULL,
     [CreateDate]   DATETIME         NULL,
-    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Users_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts] ([AccountId])
 );
+
+
 
 
 
