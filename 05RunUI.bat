1454e9a3b004d073622de93f19e003c3b79507c1
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-06 09:06:30 -0700

    Update 05RunUI.bat

--- 05RunUI.bat
+++ 05RunUI.bat
@@ -1,4 +1,7 @@
 @echo on
 TITLE Build UI Environment
 ECHO Building UI...
-START cmd.exe /k "cd C:\Program Files (x86)\Go Agent\pipelines\UI-QA\SkaldBotUI&npm run build"
+@echo before
+cd C:\Program Files (x86)\Go Agent\pipelines\UI-QA\SkaldBotUI
+@echo after
+npm run build
