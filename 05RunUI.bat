de233b14ca7f147af1e7946353d7dcdf22858cc0
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-06 09:32:39 -0700

    Delete 05RunUI.bat

--- 05RunUI.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-TITLE Build UI Environment
-ECHO Building UI...
-@echo before
-cd C:\Program Files (x86)\Go Agent\pipelines\UI-QA\SkaldBotUI
-@echo installing packages and building.
-/k "npm install @vue/cli&&npm install @vue/cli-service&&npm run build"
