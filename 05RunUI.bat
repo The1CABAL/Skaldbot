f5f97f46d111978aafcecb3dd881d534fde7969a
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-06 09:31:04 -0700

    Update 05RunUI.bat

--- 05RunUI.bat
+++ 05RunUI.bat
@@ -3,7 +3,5 @@ TITLE Build UI Environment
 ECHO Building UI...
 @echo before
 cd C:\Program Files (x86)\Go Agent\pipelines\UI-QA\SkaldBotUI
-npm install @vue/cli &&
-npm install @vue/cli-service &&
-@echo after
-npm run build
+@echo installing packages and building.
+/k "npm install @vue/cli&&npm install @vue/cli-service&&npm run build"
