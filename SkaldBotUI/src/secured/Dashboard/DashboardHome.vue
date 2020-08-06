8b597acaede0e7e02d1ac6d7997a565307243361
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-05 20:08:06 -0700

    #17 - Dashboard home text updated

--- SkaldBotUI/src/secured/Dashboard/DashboardHome.vue
+++ SkaldBotUI/src/secured/Dashboard/DashboardHome.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="DashboardHome">
-        <p>This is the home dashboard</p>
+        <p>Welcome to the admin dashboard. Select one of the options in the navigation pane to the left to start managing the application!</p>
     </div>
 </template>
 
