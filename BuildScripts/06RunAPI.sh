d9851f1a86a5b8e453044b512330f74670dcb5f9
Isaac Bly <locke19hegemon@gmail.com>
2020-08-07 20:40:53 -0700

    Changed port to match internal container

--- BuildScripts/06RunAPI.sh
+++ BuildScripts/06RunAPI.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it -p 5555:5479 --name skaldbotapi -d skaldbotapi && sleep 10 && sudo docker exec -t skaldbotapi echo "Api Container Is Running"
+sudo docker run -it -p 5555:5555 --name skaldbotapi -d skaldbotapi && sleep 10 && sudo docker exec -t skaldbotapi echo "Api Container Is Running"
