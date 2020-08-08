d95df9190c6482e1b3e21b90c1bf9f9ae61c002c
Isaac Bly <locke19hegemon@gmail.com>
2020-08-07 19:50:14 -0700

    Corrected deployment scripts

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo docker stop skaldbotcore
 sudo docker rm skaldbotcore
-#sudo docker stop skaldbotapi
-#sudo docker rm skaldbotapi
+sudo docker stop skaldbotapi
+sudo docker rm skaldbotapi
 #sudo docker stop skaldbotui
 #sudo docker rm skaldbotui
