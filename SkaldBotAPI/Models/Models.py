11355222a849569b08d91de23022acfae04e18e8
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-08 10:45:41 -0700

    #77 - Register user no longer expects email

--- SkaldBotAPI/Models/Models.py
+++ SkaldBotAPI/Models/Models.py
@@ -2,7 +2,7 @@ import collections
 
 Suggestion = collections.namedtuple('suggestion', ['typeId', 'title', 'story', 'serverid', 'discorduserid'])
 User = collections.namedtuple('user', ['username', 'password'])
-RegisterUser = collections.namedtuple('registeruser', ['accountname', 'username', 'firstname', 'lastname', 'discorduserid', 'password', 'email'])
+RegisterUser = collections.namedtuple('registeruser', ['accountname', 'username', 'firstname', 'lastname', 'discorduserid', 'password'])
 RegisterAccountUser = collections.namedtuple('registeraccountuser', ['accountid', 'username', 'firstname', 'lastname', 'discorduserid', 'password'])
 UserProfile = collections.namedtuple('userprofile', ['username', 'firstname', 'lastname', 'discorduserid', 'isactive', 'islocked', 'role'])
 StoryModel = collections.namedtuple('story', ['id', 'title', 'story', 'serverid', 'isactive'])
