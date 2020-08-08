770a7c87008373520d5bef745e945a27f00e37e4
Isaac Bly <locke19hegemon@gmail.com>
2020-08-07 19:31:35 -0700

    permissions given

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker stop skaldbotcore
-docker rm skaldbotcore
+sudo docker stop skaldbotcore
+sudo docker rm skaldbotcore
 #sudo docker stop skaldbotapi
 #sudo docker rm skaldbotapi
 #sudo docker stop skaldbotui
