3057247ec0829c0f1b4cc75e327832aa6d96a0a4
Isaac Bly <locke19hegemon@gmail.com>
2020-08-07 21:02:38 -0700

    Container networking

--- BuildScripts/07RunUI.sh
+++ BuildScripts/07RunUI.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it -p 8080:8080 --name skaldbotui -d skaldbotui --link skaldbotapi:skaldbotapi && sleep 10 && sudo docker exec -t skaldbotui echo "Api Container Is Running"
+sudo docker run -it -p 8080:8080 --name skaldbotui --link skaldbotapi:skaldbotapi -d skaldbotui && sleep 10 && sudo docker exec -t skaldbotui echo "Api Container Is Running"
