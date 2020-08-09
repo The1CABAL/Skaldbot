cb6ebe98e67be509dfe00ad694e97dc17dc29fe1
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-09 16:19:20 -0700

    attempt to cache clear of docker

--- BuildScripts/WindowsBuildScripts/00StopContainer.bat
+++ BuildScripts/WindowsBuildScripts/00StopContainer.bat
@@ -6,6 +6,7 @@ docker rm skaldbotui
 docker rm skaldbotapi
 docker rmi skaldbotui
 docker rmi skaldbotapi
+docker builder prune
 cd ../../SkaldBotUI/src/Offline
 docker build --tag skaldbotoffline "./"
 docker run --name skaldbotoffline -d -p 8080:80 skaldbotoffline
