a29afc4bd73f099ca0cfd12a005ed0a308300931
Isaac Bly <locke19hegemon@gmail.com>
2020-08-09 15:46:52 -0700

    #83 first stage reenabled

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#sudo docker stop skaldbotcore
-#sudo docker rm skaldbotcore
-#sudo docker stop skaldbotapi
-#sudo docker rm skaldbotapi
-#sudo docker stop skaldbotui
-#sudo docker rm skaldbotui
+sudo docker stop skaldbotcore
+sudo docker rm skaldbotcore
+sudo docker stop skaldbotapi
+sudo docker rm skaldbotapi
+sudo docker stop skaldbotui
+sudo docker rm skaldbotui
 sudo docker start skaldbotoffline
