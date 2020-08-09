a4242cad0fd54a1d2b5663f0183067157a48da91
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2020-08-09 13:20:28 -0700

    Maintenance Attempt

--- BuildScripts/WindowsBuildScripts/00StopContainer.bat
+++ BuildScripts/WindowsBuildScripts/00StopContainer.bat
@@ -6,3 +6,6 @@ docker rm skaldbotui
 docker rm skaldbotapi
 docker rmi skaldbotui
 docker rmi skaldbotapi
+cd ../../SkaldBotUI/Offline
+docker build --tag skaldbotoffline "./"
+docker run --name skaldbotoffline -p 8080:8080 skaldbotoffline
--- BuildScripts/WindowsBuildScripts/04RunUI.bat
+++ BuildScripts/WindowsBuildScripts/04RunUI.bat
@@ -1,3 +1,6 @@
 @echo on
 @echo Starting UI
+docker kill skaldbotoffline
+docker rm skaldbotoffline
+docker rmi skaldbotoffline
 docker run -p 8080:8080 --name skaldbotui --link skaldbotapi:skaldbotapi -d skaldbotui
