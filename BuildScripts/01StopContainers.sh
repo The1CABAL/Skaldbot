f725f1f7df467d35396a978b354e1a78fbdc8c3d
Isaac Bly <locke19hegemon@gmail.com>
2020-08-09 15:54:25 -0700

    Added sleep commands to avoid script going too fast

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo docker stop skaldbotcore || \
-sudo docker rm skaldbotcore || \
-sudo docker stop skaldbotapi || \
-sudo docker rm skaldbotapi || \
-sudo docker stop skaldbotui || \
-sudo docker rm skaldbotui || \
+sudo docker stop skaldbotcore && sleep 2|| \
+sudo docker rm skaldbotcore && sleep 2 || \
+sudo docker stop skaldbotapi && sleep 2 || \
+sudo docker rm skaldbotapi && sleep 2|| \
+sudo docker stop skaldbotui && sleep 2|| \
+sudo docker rm skaldbotui && sleep 2|| \
 sudo docker start skaldbotoffline || \
 echo "All Containers stopped and maintenance page is active."
