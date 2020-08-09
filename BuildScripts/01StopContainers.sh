1383081cbf5e5742a5d8bb42c459e5c5ed267f8e
Isaac Bly <locke19hegemon@gmail.com>
2020-08-09 16:09:41 -0700

    first script optimization

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo docker stop skaldbotcore && sleep 2|| \
-sudo docker rm skaldbotcore && sleep 2 || \
-sudo docker stop skaldbotapi && sleep 2 || \
-sudo docker rm skaldbotapi && sleep 2|| \
-sudo docker stop skaldbotui && sleep 2|| \
-sudo docker rm skaldbotui && sleep 2|| \
-sudo docker start skaldbotoffline || \
+sudo docker stop skaldbotcore || \
+sleep 3 && sudo docker rm skaldbotcore  || \
+sleep 3 && sudo docker stop skaldbotapi  || \
+sleep 3 && sudo docker rm skaldbotapi || \
+sleep 3 && sudo docker stop skaldbotui || \
+sleep 3 && sudo docker rm skaldbotui || \
+sleep 3 && sudo docker start skaldbotoffline  || \
 echo "All Containers stopped and maintenance page is active."
