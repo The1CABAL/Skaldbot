faa527764ae9962242fb87f960ac63181b6748e1
Isaac Bly <locke19hegemon@gmail.com>
2020-08-09 15:39:24 -0700

    #83 deployment changes

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -5,3 +5,4 @@ sudo docker stop skaldbotapi
 sudo docker rm skaldbotapi
 sudo docker stop skaldbotui
 sudo docker rm skaldbotui
+sudo docker run -it -p 8080:80 --name skaldbotoffline -d skaldbotoffline
--- BuildScripts/07RunUI.sh
+++ BuildScripts/07RunUI.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo docker stop skaldbotoffline
 sudo docker run -it -p 8080:8080 --name skaldbotui --link skaldbotapi:skaldbotapi -d skaldbotui && sleep 10 && sudo docker exec -t skaldbotui echo "Api Container Is Running"
