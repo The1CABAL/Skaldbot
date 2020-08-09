34d5c1d11bd375b7cfa9002f21fb49da330f582e
Isaac Bly <locke19hegemon@gmail.com>
2020-08-09 15:50:22 -0700

    removed fail conditions from the first stage

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo docker stop skaldbotcore
-sudo docker rm skaldbotcore
-sudo docker stop skaldbotapi
-sudo docker rm skaldbotapi
-sudo docker stop skaldbotui
-sudo docker rm skaldbotui
-sudo docker start skaldbotoffline
+sudo docker stop skaldbotcore || \
+sudo docker rm skaldbotcore || \
+sudo docker stop skaldbotapi || \
+sudo docker rm skaldbotapi || \
+sudo docker stop skaldbotui || \
+sudo docker rm skaldbotui || \
+sudo docker start skaldbotoffline || \
+echo "All Containers stopped and maintenance page is active."
