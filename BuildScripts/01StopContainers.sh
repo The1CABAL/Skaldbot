12dc4e2380920db55d6275acaae33c48eab47a86
Isaac Bly <locke19hegemon@gmail.com>
2020-08-09 15:41:18 -0700

    #83 first stage correction

--- BuildScripts/01StopContainers.sh
+++ BuildScripts/01StopContainers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo docker stop skaldbotcore
-sudo docker rm skaldbotcore
-sudo docker stop skaldbotapi
-sudo docker rm skaldbotapi
-sudo docker stop skaldbotui
-sudo docker rm skaldbotui
-sudo docker run -it -p 8080:80 --name skaldbotoffline -d skaldbotoffline
+#sudo docker stop skaldbotcore
+#sudo docker rm skaldbotcore
+#sudo docker stop skaldbotapi
+#sudo docker rm skaldbotapi
+#sudo docker stop skaldbotui
+#sudo docker rm skaldbotui
+sudo docker start skaldbotoffline
