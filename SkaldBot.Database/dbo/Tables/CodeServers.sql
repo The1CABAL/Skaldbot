cebade3b947b01f88f83458a30627f48c6365312
Isaac Bly <locke19hegemon@gmail.com>
2020-08-10 15:43:03 -0700

    Fix deployment issues

--- SkaldBot.Database/dbo/Tables/CodeServers.sql
+++ SkaldBot.Database/dbo/Tables/CodeServers.sql
@@ -6,6 +6,7 @@
     [DailyWisdom] BIT            CONSTRAINT [DF_CodeServers_DailyWisdom] DEFAULT ((1)) NOT NULL,
     [UpdateDate]  DATETIME       CONSTRAINT [DF_CodeServers_UpdateDate] DEFAULT (getdate()) NULL,
     [WeeklyStory] BIT NOT NULL DEFAULT ((0)), 
+    [IsActive] BIT NOT NULL DEFAULT ((0)), 
     CONSTRAINT [PK_CodeServers] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_CodeServers_Accounts] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts] ([AccountId])
 );
