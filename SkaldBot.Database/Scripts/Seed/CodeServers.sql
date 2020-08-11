30ab8a2baaf59cf88fada98b4ad42ba29c059890
Devin <51932866+DevinWalkup@users.noreply.github.com>
2020-08-10 23:25:16 -0700

    #84 - Update to server seed to add account id

--- SkaldBot.Database/Scripts/Seed/CodeServers.sql
+++ SkaldBot.Database/Scripts/Seed/CodeServers.sql
@@ -3,20 +3,20 @@ SET IDENTITY_INSERT dbo.CodeServers ON
 
 IF NOT EXISTS (SELECT * FROM CodeServers WHERE Id = 1)
 	BEGIN
-		INSERT INTO CodeServers (id, ServerId, Nickname, DailyWisdom) VALUES (1, '725880649356935192', 'Production', 1)
+		INSERT INTO CodeServers (id, ServerId, Nickname, AccountId, DailyWisdom, WeeklyStory) VALUES (1, '725880649356935192', 'Production', 1, 1, 1)
 	END
 ELSE
 	BEGIN
-		UPDATE CodeServers SET ServerId = '725880649356935192', Nickname = 'Production', DailyWisdom = 1 WHERE id = 1
+		UPDATE CodeServers SET ServerId = '725880649356935192', DailyWisdom = 1, AccountId = 1, WeeklyStory = 1 WHERE id = 1
 	END
 
 IF NOT EXISTS (SELECT * FROM CodeServers WHERE Id = 2)
 	BEGIN
-		INSERT INTO CodeServers (id, ServerId, Nickname, DailyWisdom) VALUES (2, '726640547019751458', 'Test', 1)
+		INSERT INTO CodeServers (id, ServerId, Nickname, AccountId, DailyWisdom, WeeklyStory) VALUES (2, '726640547019751458', 'Test', 1, 1, 1)
 	END
 ELSE
 	BEGIN
-		UPDATE CodeServers SET ServerId = '726640547019751458', Nickname = 'Test', DailyWisdom = 0 WHERE id = 2
+		UPDATE CodeServers SET ServerId = '726640547019751458', AccountId = 1, DailyWisdom = 0, WeeklyStory = 0 WHERE id = 2
 	END
 
 SET IDENTITY_INSERT dbo.CodeServers OFF
